a4333885c074d3739a173f08f99c5c07e05e9031
eshwar rachala <eshwar.appdev@gmail.com>
2017-06-15 21:39:32 -0400

    updated resume

--- src/projects.js
+++ src/projects.js
@@ -1,12 +1,16 @@
 const projects = [{
         'title': 'Leading Indicators',
         'org': 'Noblesoft Solutions(Florida Blue)',
-        'duration': '10/4/2010 - present'
+        'duration': '10/4/2010 - present',
+        'desc': `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+        sanctus est Lorem ipsum dolor sit amet`
     },
     {
         'title': 'Leading Indicators',
         'org': 'Noblesoft Solutions(Florida Blue)',
-        'duration': '10/4/2010 - present'
+        'duration': '10/4/2010 - present',
+        'desc': `Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+        sanctus est Lorem ipsum dolor sit amet`
     }
 ]
 
